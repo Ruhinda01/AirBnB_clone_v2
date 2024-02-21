00d073b8e69fd3ed4ee8abd50f0510428d754199
Ruhinda Roderick Izooba <rickyruhinda@gmail.com>
2024-02-21 19:00:24 +0300

    dev_sql

---
+++ setup_mysql_dev.sql
@@ -0,0 +1,9 @@
+-- creates a database hbnb_dev_db
+-- creates a new user hbnb_dev in localhost
+-- grants priviledges to database hbhb_dev_db
+-- grants SELECT priviledge to database performance_schema
+-- database and user shouldn't fail if already exists
+CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
+CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
+GRANT ALL PRIVILEDGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
+GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
---
+++ tests/test_console.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+"""Unittest for the console"""
+
+import unittest
+import shlex
+from console import HBNBCommand
+from models.base_model import BaseModel
+from models.__init__ import storage
+from models.user import User
+from models.place import Place
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.review import Review
+
+class test_HBNBCommand(unittest.TestCase):
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+if __name__ == '__main__':
+    unittest.main()
