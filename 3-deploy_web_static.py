b4a5f20106a4b3d641ff28dec4851427320569ea
Ruhinda Roderick Izooba <rickyruhinda@gmail.com>
2024-03-21 05:12:20 -0700

    trial

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -4,10 +4,9 @@ Fabric script that creates and distributes an archive to
 my web servers
 """
 
-import os
+from os.path import exists, isdir
 from fabric.api import *
 from datetime import datetime
-
 
 env.hosts = ['54.237.97.154', '54.224.31.23']
 
@@ -17,7 +16,7 @@ def deploy():
     Creates and distributes an archive
     """
     archive_path = do_pack()
-    if os.path.exists(archive_path) is False:
+    if exists(archive_path) is False:
         return False
     return do_deploy(archive_path)
 
@@ -26,15 +25,16 @@ def do_pack():
     """
     Pack web_static content
     """
-    local("mkdir -p versions")
-    current_datetime = datetime.now()
-    formatted_datetime = current_datetime.strftime("%Y%m%d%H%M%S")
-    path = "versions/web_static_" + formatted_datetime + ".tgz"
-    cmd = "tar -cvzf {} web_static".format(path)
-
-    if local(cmd).failed:
+    try:
+        if isdir("versions") is False:
+            local("mkdir versions")
+        current_datetime = datetime.now()
+        formatted_datetime = current_datetime.strftime("%Y%m%d%H%M%S")
+        path = "versions/web_static_" + formatted_datetime + ".tgz"
+        cmd = "tar -cvzf {} web_static".format(path)
+        return path
+    except:
         return None
-    return path
 
 
 def do_deploy(archive_path):
@@ -43,30 +43,21 @@ def do_deploy(archive_path):
     Arg:
         archive_path (file)
     """
-    if os.path.exists(archive_path) is False:
+    if exists(archive_path) is False:
         return False
 
-    path = "/data/web_static/releases/"
-    name = archive_path.split("/")[-1]
-    no_exten = name.rsplit(".", 1)[0]
-
-    if put(archive_path, "/tmp/").failed:
+    try:
+        path = "/data/web_static/releases/"
+        name = archive_path.split("/")[-1]
+        no_exten = name.split(".")[0]
+        put(archive_path, '/tmp/')
+        run('mkdir -p {}{}/'.format(path, no_exten))
+        run('tar -xzf /tmp/{} -C {}{}/'.format(name, path, no_exten))
+        run('rm /tmp/{}'.format(name))
+        run('mv {0}{1}/web_static/* {0}(1}/'.format(path, no_exten))
+        run('rm -rf {}{}/web_static'.format(path, no_exten))
+        run('rm -rf /data/web_static/current')
+        run('ln -s {}{}/ /data/web_static/current'.format(path, no_exten))
+        return True
+    except:
         return False
-    if run("sudo mkdir -p {}{}/".format(path, no_exten)).failed:
-        return False
-    if run("sudo tar -xzf /tmp/{} -C {}{}/".
-            format(name, path, no_exten)).failed:
-        return False
-    if run("sudo rm /tmp/{}".format(name)).failed:
-        return False
-    if run("sudo mv {0}{1}/web_static/* {0}{1}/".
-            format(path, no_exten)).failed:
-        return False
-    if run("sudo rm -rf {}{}/web_static".format(path, no_exten)).failed:
-        return False
-    if run("sudo rm -rf /data/web_static/current").failed:
-        return False
-    if run("sudo ln -s {}{}/ /data/web_static/current".
-            format(path, no_exten)).failed:
-        return False
-    return True
