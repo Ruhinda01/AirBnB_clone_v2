02b1929eacdd228d16a8cf00009fb1d677bb9de2
Ruhinda Roderick Izooba <rickyruhinda@gmail.com>
2024-03-24 23:15:19 +0300

    number 0 and readme

---
+++ web_flask/0-hello_route.py
@@ -0,0 +1,14 @@
+from flask import Flask
+"""Script that starts a Flask web application"""
+
+app = Flask(__name__)
+
+@app.route('/', strict_slashes=False)
+def hello_hbnb():
+    """
+    Returns "Hello HBNB!"
+    """
+    return "Hello HBNB!"
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
